14190a7bffa01b0217d2d81a215d5cd70b7b9991
suchoss <suchoss@gmail.com>
2021-06-02 15:08:35 +0200

    Scaffold new project

---
+++ RazorPageSupportTest/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using RazorPageSupportTest.Data;
+
+[assembly: HostingStartup(typeof(RazorPageSupportTest.Areas.Identity.IdentityHostingStartup))]
+namespace RazorPageSupportTest.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
